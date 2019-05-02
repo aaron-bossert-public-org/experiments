a3b3446aba23c85114e222d91e93f90db860a6f4
Aaron Bossert <aaron.bossert.gpu@gmail.com>
2019-05-01 18:58:48 -0700

    Add warnings as errors, emit all warnings

---
+++ library/atmos/third_party/gli_includes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#pragma warning( push )
+#pragma warning( disable : 4458)
+#pragma warning( disable : 4100)
+#pragma warning( disable : 4201)
+
+#include <atmos/third_party/glm_includes.h>
+#include <atmos/third_party/gli/gli/gli.hpp>
+
+#pragma warning( pop ) 
---
+++ library/atmos/third_party/glm_includes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#pragma warning( push )
+#pragma warning( disable : 4201)
+#pragma warning( disable : 4127)
+
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/hash.hpp>
+
+#pragma warning( pop ) 
