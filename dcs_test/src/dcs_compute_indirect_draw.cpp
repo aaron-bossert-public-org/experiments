69db5a822d4cd2ffdc0e5253172bbce2fc65d32a
Aaron Bossert <aaron.bossert.gpu@gmail.com>
2019-11-27 12:56:20 -0800

    add instanced geometry to compute indirect example

--- dcs_test/src/dcs_compute_indirect_draw.cpp
+++ dcs_test/src/dcs_compute_indirect_draw.cpp
@@ -14,6 +14,13 @@
 
 using namespace igpu;
 
+namespace
+{
+	const int32_t s_instance_length = 24;
+	const int32_t s_instance_volume =
+		s_instance_length * s_instance_length * s_instance_length;
+}
+
 std::unique_ptr< dcs_compute_indirect_draw > dcs_compute_indirect_draw::make(
 	const config& cfg )
 {
@@ -65,7 +72,7 @@ std::unique_ptr< dcs_compute_indirect_draw > dcs_compute_indirect_draw::make(
 			cfg.context->make_shared(
 				fragment_shader::config{},
 				dcs_utils::load_mem( cfg.fragment_path.c_str() ) ),
-		} );
+			{ { { "MAX_INSTANCE_ID", s_instance_volume } } } } );
 
 		st.model = dcs_utils::load_model(
 			cfg.context.get(),
@@ -83,9 +90,12 @@ std::unique_ptr< dcs_compute_indirect_draw > dcs_compute_indirect_draw::make(
 		for ( size_t msh = 0; msh < st.model.meshes.size(); ++msh )
 		{
 			const auto& mesh = st.model.meshes[msh];
-			for ( size_t i = mesh.base_vertex; i < mesh.vertex_count; ++i )
+			for ( size_t i = mesh.base_vertex;
+				  i < mesh.vertex_count + mesh.base_vertex;
+				  ++i )
 			{
 				color_view[i] = mesh.material_color;
+				color_view[i].a = (float)msh;
 			}
 		}
 
@@ -108,6 +118,30 @@ dcs_compute_indirect_draw::dcs_compute_indirect_draw(
 	, _st( std::move( st ) )
 {
 	update();
+	struct
+	{
+		glm::mat4 model[s_instance_length][s_instance_length]
+					   [s_instance_length];
+	}* instance_ubo = nullptr;
+
+	_st.instance_data->map( &instance_ubo );
+
+	auto& mats = instance_ubo->model;
+	float scale = 5.f;
+	float offset = scale * s_instance_length * -0.5f;
+
+	glm::mat4x4 ident = glm::mat4( 1.0f );
+	for ( size_t z = 0; z < s_instance_length; ++z )
+		for ( size_t y = 0; y < s_instance_length; ++y )
+			for ( size_t x = 0; x < s_instance_length; ++x )
+			{
+				ident[3][0] = x * scale + offset;
+				ident[3][1] = y * scale + offset;
+				ident[3][2] = z * scale + offset;
+				mats[x][y][z] = ident;
+			}
+
+	_st.instance_data->unmap();
 
 	_st.opaque_batch = cfg.context->make_shared( opaque_batch::config{
 		cfg.context->back_buffer(),
@@ -116,29 +150,26 @@ dcs_compute_indirect_draw::dcs_compute_indirect_draw(
 		} } ),
 	} );
 
-	for ( const auto& mesh : _st.model.meshes )
-	{
-		_st.batch_bindings.push_back( _st.opaque_batch->make_binding( {
-			_st.program,
-			_st.render_states,
-			_st.model.geometry,
-			cfg.context->make_shared( primitives::config{ {
-				{ "texSampler", _st.texture },
-			} } ),
-			cfg.context->make_shared( primitives::config{ {
-				{ "instance_data", _st.instance_data },
-			} } ),
-		} ) );
+	_st.batch_bindings.push_back( _st.opaque_batch->make_binding( {
+		_st.program,
+		_st.render_states,
+		_st.model.geometry,
+		cfg.context->make_shared( primitives::config{ {
+			{ "texSampler", _st.texture },
+		} } ),
+		cfg.context->make_shared( primitives::config{ {
+			{ "instance_data", _st.instance_data },
+		} } ),
+	} ) );
 
-		_st.batch_bindings.back()->instance_batch().draw_params(
-			igpu::draw_parameters{
-				mesh.index_count,
-				1,
-				mesh.first_index,
-				mesh.base_vertex,
-				0,
-			} );
-	}
+	_st.batch_bindings.back()->instance_batch().draw_params(
+		igpu::draw_parameters{
+			_st.model.meshes.back().index_count,
+			s_instance_volume,
+			_st.model.meshes.back().first_index,
+			_st.model.meshes.back().base_vertex,
+			0,
+		} );
 }
 
 dcs_compute_indirect_draw::~dcs_compute_indirect_draw()
@@ -162,30 +193,6 @@ bool dcs_compute_indirect_draw::advance()
 
 void dcs_compute_indirect_draw::update()
 {
-	// update view matrices
-	auto back_buffer = _cfg.context->back_buffer();
-	auto res = back_buffer->res();
-
-	struct
-	{
-		alignas( 16 ) glm::mat4 view;
-		alignas( 16 ) glm::mat4 proj;
-	}* batch_ubo = nullptr;
-
-	_st.batch_data->map( &batch_ubo );
-
-	batch_ubo->view = glm::lookAt(
-		glm::vec3( 1.0f, 2.0f, 2.0f ),
-		glm::vec3( 0.0f, 0.0f, 0.0f ),
-		glm::vec3( 0.0f, 0.0f, 1.0f ) );
-	batch_ubo->proj = glm::perspective(
-		glm::radians( 45.0f ),
-		res.x / (float)res.y,
-		0.1f,
-		10.0f );
-
-	_st.batch_data->unmap();
-
 	// update model transform
 	static auto start_time = std::chrono::high_resolution_clock::now();
 	auto current_time = std::chrono::high_resolution_clock::now();
@@ -193,19 +200,37 @@ void dcs_compute_indirect_draw::update()
 					 current_time - start_time )
 					 .count();
 
-	struct
-	{
-		alignas( 16 ) glm::mat4 model;
-	}* instance_ubo = nullptr;
+	// update view matrices
+	auto back_buffer = _cfg.context->back_buffer();
+	auto res = back_buffer->res();
 
-	_st.instance_data->map( &instance_ubo );
-
-	instance_ubo->model = glm::rotate(
+	auto rot = glm::rotate(
 		glm::mat4( 1.0f ),
-		time * glm::radians( 90.0f ),
+		time * glm::radians( 90.0f ) / 16.f,
 		glm::vec3( 0.0f, 0.0f, 1.0f ) );
 
-	_st.instance_data->unmap();
+	auto look = glm::lookAt(
+		glm::vec3( 50.0f, 0.0f, 50.0f ),
+		glm::vec3( 0.0f, 0.0f, 0.0f ),
+		glm::vec3( 0.0f, 0.0f, 1.0f ) );
+
+	struct
+	{
+		glm::mat4 view;
+		glm::mat4 proj;
+	}* batch_ubo = nullptr;
+
+	_st.batch_data->map( &batch_ubo );
+
+	batch_ubo->view = look * rot;
+
+	batch_ubo->proj = glm::perspective(
+		glm::radians( 60.0f ),
+		res.x / (float)res.y,
+		0.5f,
+		1000.0f );
+
+	_st.batch_data->unmap();
 }
 
 void dcs_compute_indirect_draw::handle_input()
